634443753d24a6e0f207fe7a69dabc0627aae60e
user <user@CEMAL>
2021-03-31 15:12:07 +0300

    New

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,45 +14,45 @@ namespace ConsoleUI
             //CarTest();
             //BrandTest();
             //ColorTest();
-            AddUser();
+            //AddUser();
             //AddRental();
         }
-        private static void AddRental()
-        {
-            RentalManager rentalManager = new RentalManager(new EfRentalDal());
-            rentalManager.Add(new Rental { Id = 1, CarId = 2, CustomerId = 5, RentDate = DateTime.Now } );
-        }
+        //private static void AddRental()
+        //{
+        //    RentalManager rentalManager = new RentalManager(new EfRentalDal());
+        //    rentalManager.Add(new Rental { Id = 1, CarId = 2, CustomerId = 5, RentDate = DateTime.Now } );
+        //}
 
-        private  static void AddUser()
-        {
-            UserManager userManager = new UserManager(new EfUserDal());
-            userManager.Add(new User { Id = 7, FirstName = "Cemal", LastName = "Taşkın", Email = "cemaltaskin@gmail.com", Password = "123456" });
-        }
+        //private  static void AddUser()
+        //{
+        //    UserManager userManager = new UserManager(new EfUserDal());
+        //    userManager.Add(new User { Id = 7, FirstName = "Cemal", LastName = "Taşkın", Email = "cemaltaskin@gmail.com", Password = "123456" });
+        //}
 
-        private static void ColorTest()
-        {
-            ColorManager colorManager = new ColorManager(new EfColorDal());
-            foreach (var color in colorManager.GetAll().Data)
-            {
-                Console.WriteLine(color.ColorId);
-            }
+        //private static void ColorTest()
+        //{
+        //    ColorManager colorManager = new ColorManager(new EfColorDal());
+        //    foreach (var color in colorManager.GetAll().Data)
+        //    {
+        //        Console.WriteLine(color.ColorId);
+        //    }
               
-        }
+        //}
 
-        private static void BrandTest()
-        {
-            BrandManager brandManager = new BrandManager(new EfBrandDal());
-            foreach (var brand in brandManager.GetAll().Data)
-            {
-                Console.WriteLine(brand.BrandName);
-            }
-        }
+        //private static void BrandTest()
+        //{
+        //    BrandManager brandManager = new BrandManager(new EfBrandDal());
+        //    foreach (var brand in brandManager.GetAll().Data)
+        //    {
+        //        Console.WriteLine(brand.BrandName);
+        //    }
+        //}
 
-        private static void CarTest()
-        {
-            CarManager carManager = new CarManager(new EfCarDal());
-            carManager.Add(new Car { Id = 5, BrandId = 3, ColorId = 3, ModelYear = 2021, DailyPrice = 250, Description = "Otomatik" });
+        //private static void CarTest()
+        //{
+        //    CarManager carManager = new CarManager(new EfCarDal());
+        //    carManager.Add(new Car { Id = 5, BrandId = 3, ColorId = 3, ModelYear = 2021, DailyPrice = 250, Description = "Otomatik" });
 
-        }
+        //}
     }
 }
