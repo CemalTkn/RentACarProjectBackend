5b238b5b3405d52f777920e6c7dd7fb6702ced59
user <user@CEMAL>
2021-06-15 22:23:47 +0300

    New

---
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator:AbstractValidator<Rental>
+    {
+        public RentalValidator()
+        {
+            RuleFor(r => r.CarId).NotEmpty();
+            RuleFor(r => r.CustomerId).NotEmpty();
+            RuleFor(r => r.RentDate)
+                .Cascade(CascadeMode.Continue)
+                .Must(BeValidDate).WithMessage("Invalid Date Of Rent")
+                .NotEmpty();
+        }
+
+        private bool BeValidDate(DateTime date)
+        {
+            int CurrentYear = DateTime.Now.Year;
+            int DateOfRentYear = date.Year;
+
+            if (DateOfRentYear <= CurrentYear && DateOfRentYear > (CurrentYear - 120))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,44 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .MinimumLength(2);
+
+
+            RuleFor(u => u.LastName)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .MinimumLength(2);
+
+
+            RuleFor(u => u.Email)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .Must(Contain).WithMessage("Your Mail address must Contain '@' character")
+                .EmailAddress();
+
+
+        }
+
+        private bool Contain(string arg)
+        {
+            if (arg.Contains("@"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
